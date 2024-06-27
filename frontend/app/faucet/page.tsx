f0b308f696f4ce895970c2e47cd322eadad72d85
sunhaoran <1208818417@qq.com>
2024-06-27 23:12:55 +0800

    Add  To Wallte

--- frontend/app/faucet/page.tsx
+++ frontend/app/faucet/page.tsx
@@ -17,15 +17,17 @@ import {getTokenAddrByNetworkId} from "@/app/lib/utils/getAddrByNetwork"
 import { transfersQuery } from "@/app/lib/api/thegraph";
 import {truncateStr} from "@/app/lib/utils/utils"
 import { useQuery } from '@apollo/client';
-
+import { watchAssetERC20 } from "@/app/lib/utils/watchAsset"
 export default function Faucet(){
     const { address,isConnected } = useAccount();
+    const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
     const chainId = useChainId();
     const [balance,setBalance] = useState("0")
     const [transfers,setTransfers] = useState([]);
+    const tonkenAddr = getTokenAddrByNetworkId(chainId.toString());
     const wagmiContractConfig = {
         abi:tokenAbi,
-        address: getTokenAddrByNetworkId(chainId.toString())
+        address: tonkenAddr
     }
     const { loading, error:qe, data } = useQuery(transfersQuery,{
         variables:{address}
@@ -87,9 +89,15 @@ export default function Faucet(){
           account:address,
         })
     }
-    
+    async function addToWallte() {
+        watchAssetERC20(tonkenAddr,"OBT")
+    }
     return(<>
         <div className="flex flex-col items-center justify-between p-15 my-10">
+        <div className="flex flex-row p-5 justify-between items-center">
+                    <p className="px-4 font-bold text-2xl">OBT Faucet</p>
+                    <Button color="secondary"  onClick={addToWallte} >Add OBT To Wallte</Button>
+                </div>
             <Card className="col-span-12 w-[300px] h-[350px] bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
                 <CardBody>
                     Balance: 
@@ -97,7 +105,7 @@ export default function Faucet(){
                 </CardBody>
             <CardFooter className="absolute bg-black/60 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
                 <Button onClick={faucet} isLoading={isPending}>Faucet</Button>
-                <span className=" text-white px-3">0.1 Sepolia OBT/day.</span>
+                <span className=" text-white px-3">0.1 OBT/day.</span>
             </CardFooter>
             </Card>
             <div className="w-full max-w-[300px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
@@ -111,11 +119,14 @@ export default function Faucet(){
                 label="Assigned to"
                 variant="flat"
             >
-                {!loading && data.transfers.map((item:{id:string,to:string,from:string,value:bigint}) => (
+                {!loading && data && data.transfers.map((item:{id:string,to:string,from:string,value:bigint}) => (
                 <ListboxItem key={item.id} textValue={item.to}>
                     <div className="flex gap-2 items-center">
                     <div className="flex flex-col">
-                        <span className="text-small">{truncateStr(item.from,15)+" => " + truncateStr(item.to,15)}</span>
+                        <span className="text-small">
+                            {item.from===ZERO_ADDR && <>faucet</>}
+                            {item.from!==ZERO_ADDR && truncateStr(item.from,15)+" => " + truncateStr(item.to,15)}
+                        </span>
                         <span className="text-tiny text-default-400">
                             {formatEther(item.value)} OBT
                         </span>
